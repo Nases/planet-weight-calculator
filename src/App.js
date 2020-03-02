a17e46a60ee20e28d1c0c7523d68673c47032163
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-01 19:11:45 -0800

    🌎

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import planets from './assets/planets.json'
 import Planet from './components/Planet'
 import Footer from './components/Footer'
 
 function App() {
+
+  const [weightInput, setWeightInput] = useState(150)
+
+  function weightChange(e) {
+    const re = /^[1-9]\d{0,2}(?:\,\d{1,3})?$/;
+    if (e.target.value === '' || re.test(e.target.value)) {
+      setWeightInput(e.target.value)
+    }
+  }
+
   return (
     <div>
+      <div>
+        <p className="text-gray-300 inline text-xl">Enter your weight: </p>
+        <input type="text" onChange={weightChange} value={weightInput} />
+      </div>
       <div className="grid grid-cols-4">
-        {planets.map(value => (<Planet name={value.name} desc={value.description} img={process.env.PUBLIC_URL + value.img} NASALink={value.NASALink} />))}
+        {planets.map(value => (
+          <Planet
+            name={value.name}
+            desc={value.description}
+            img={process.env.PUBLIC_URL + value.img}
+            NASALink={value.NASALink}
+            surfaceGravityMultiplier={value.surfaceGravityMultiplier}
+            weightInput={weightInput}
+          />
+        ))}
       </div>
       <Footer />
     </div>
--- src/components/planet.js
+++ src/components/planet.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default ({ name, desc, surfaceGravity, img, NASALink }) => {
+export default ({ name, desc, surfaceGravityMultiplier, img, NASALink, weightInput }) => {
+
+  const [weight, setWeight] = useState(1)
+
+  function calculateWeight(input) {
+    setWeight(Math.round(input * surfaceGravityMultiplier))
+  }
+
+  useEffect(() => {
+    calculateWeight(weightInput)
+  }, [weightInput])
 
   return (
     <div className="bg-opacity-on-hover mx-auto px-2 pb-2">
@@ -11,7 +21,8 @@ export default ({ name, desc, surfaceGravity, img, NASALink }) => {
           &nbsp;
           &#8594;
           &nbsp;
-          <input className="bg-transparent text-blue-300 inline w-10" type="text" defaultValue="40" />
+          <input className="bg-transparent text-blue-300 inline w-10" type="text" value={weight} disabled />
+          &nbsp;
           pounds
           </h2>
         <p className="text-gray-600">{desc}</p>
