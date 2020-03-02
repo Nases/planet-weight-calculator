10eb9723775b497ccd0792e53c99890a89c79e99
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-01 16:31:35 -0800

    🌌

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import mercuryImg from './assets/img/1_mercury.jpg'
 import planets from './assets/planets.json'
+import Planet from './components/planet'
 
 function App() {
   return (
-    <div>
-      hello
+    <div className="px-20 py-16">
+
+      {planets.map(value => console.log(value))}
+      {/* {planets.map(value => value.map(value => console.log(value)))} */}
+
+      <Planet img={mercuryImg} />
+
+      <footer className="text-gray-500 text-center">
+        &copy;
+        &nbsp;
+        {new Date().getFullYear()}
+        &nbsp;
+        <a className="hover:text-white" href="https://hasansefaozalp.com/" target="_blank" rel="noopener noreferrer">Hasan Sefa Ozalp</a>
+        &nbsp;
+        |
+        &nbsp;
+        <a className="hover:text-white" href="https://github.com/Nases/planet-weight-calculator" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+      </footer>
     </div>
   );
 }
---
+++ src/components/planet.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default ({ name, desc, surfaceGravity, img }) => {
+  return (
+    <div className="px-20 py-16">
+      <div className="w-64">
+        <img src={img} alt={name} />
+        <h2 className="text-2xl text-gray-200">
+          {name}
+          &#8594;
+          &nbsp;
+            <input className="bg-transparent text-blue-300 inline w-10" type="text" defaultValue="40" />
+          pounds</h2>
+        <p className="text-gray-400">{desc}</p>
+      </div>
+    </div>
+  )
+}
